4f2edf1a46693d38e4f3eb3a1d477d3a0da6a093
shrivastavradhika9-lgtm <shrivastavradhika9-lgtm@gmail.com>
2025-12-20 09:17:13 +0530

    Fix newline formatting and division handling in task1

--- task1.py
+++ task1.py
@@ -1,6 +1,3 @@
 a=int(input("enter the first number"))
 b=int(input("enter the second number"))
-print(f'addition:{a+b}')
-print(f'subtraction:{a-b}')
-print(f'multiplication:{a*b}')
-print(f'devision:{a/b}')
+print(f'addition:{a+b}\nsubtraction: {a-b}\nmultiplication: {a*b}\ndevision:{a/b}')
